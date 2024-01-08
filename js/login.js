2c7a8e4defb67304b58edd7b7e0a92d46ed351bf
bob n.c <motar3000@hotmail.com>
2024-01-08 15:00:48 +0100

    faltaba una almuadilla de la id

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
-    const loginform = document.querySelector('loginform')
+    const loginform = document.querySelector('#loginform')
     loginform.addEventListener('submit', (e)=>{
     e.preventDefault()
     const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
     const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user=> user.email === email && user.password === password)
+    const validUser = Users.find(user => user.email === email && user.password === password)
     if(!validUser){
         return alert('Usuario y/o contraseña incorrectos!')
     }
