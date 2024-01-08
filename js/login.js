6284e77e84a59ac7327e5bac95d5c0b18dd2f91b
bob n.c <motar3000@hotmail.com>
2024-01-08 14:49:30 +0100

    se añadido la almuadilla a la id

--- js/login.js
+++ js/login.js
@@ -2,8 +2,8 @@
     loginform.addEventListener('submit', (e)=>{
     e.preventDefault()
     const email = document.querySelector('#email').value
-    const password = document.querySelector('password').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const password = document.querySelector('#password').value
+    const Users = JSON.parse(localStorage.getItem('#users')) || []
     const validUser = Users.find(user=> user.email === email && user.password === password)
     if(!validUser){
         return alert('Usuario y/o contraseña incorrectos!')
