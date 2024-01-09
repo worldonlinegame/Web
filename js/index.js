9f70c35c449f1429c390a36d8a097cd1cb8d2926
bob n.c <motar3000@hotmail.com>
2024-01-09 22:49:28 +0100

    he trasladado la función al js

--- js/index.js
+++ js/index.js
@@ -7,3 +7,11 @@ if(!user){
 
 
 
+const inputFile = document.getElementById("myImage")
+const myImage = document.getElementById("myFile")
+
+inputFile.addEventListener("change", e=>{
+    console.log(myImage.file)
+    const blob = inputFile.files[0]
+    myFile.src = URL.createObjectURL(blob)
+})
