e00ddad6c5109df7316d50f26f74fb633a9638aa
bob n.c <motar3000@hotmail.com>
2024-01-10 19:06:17 +0100

    cambio

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@
     const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
     const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user => user.email === email && user.password === password)
+    const validUser = user.find(user => user.email === email && user.password === password)
     if(!validUser){
         return alert('Usuario y/o contraseña incorrectos!')
     }
