ea8f86104666d8c9ba7ec80aab9b8f429f629e9e
worldonlinegame <motar3000@hotmail.com>
2024-01-26 12:58:15 +0100

    se ha añadido una función subir img en index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*
+ Esta funcion carga la imagen pero no hace nada ya que la store del navegador no tiene subir imagenes
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
     window.location.href = 'login.html'
@@ -9,4 +11,24 @@ logout.addEventListener('click', ()=>{
     localStorage.removeItem('login_success')
     window.location.href = 'index.html'
 })
+*/
+
+
 /*Crear base de datos "blog" con javascript*/
+
+
+
+/* Funcion para subir imagenes */
+import  Express from 'express';
+import multer from 'multer';
+
+const PORT = 3000;
+
+const multerUpload = multer( );
+const express = Express();
+
+expressApp.post('/upload');
+
+
+express.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
+
