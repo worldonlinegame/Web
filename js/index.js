c6d5b38b841bf32336b7decd63e78a5ebc183774
worldonlinegame <motar3000@hotmail.com>
2024-01-26 20:15:17 +0100

    provando sub img

--- js/index.js
+++ js/index.js
@@ -16,53 +16,31 @@ logout.addEventListener('click', ()=>{
 
 /*Crear base de datos "blog" con javascript*/
 
+/*
+la url de github es: https://worldonlinegame.github.io/Web/public/fcbook.png
+*/
+
 
 
 /* Funcion para subir imagenes */
 
 const express = require('express');
+const multer = require('multer');
 
+const app = express();
+
+app.post('/uploads', upload.single('myImage'), (req, res)=>{
+    console.log(req.file);
+    res.send('Imagen subida');
+})
+
+app.listen(3000, () => console.log('Servidor iniciado en el puerto 3000'));
 //import  express from 'express';
 
 import multer from 'multer';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const PORT = 3000;
-const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
-const MIMETYPES = ['image/jpeg', 'image/png', 'image/gif'];
 
-const multerUpload = multer( {
-   storage: multer.diskStorage({
-       destination: join(CURRENT_DIR, '../uploads'),
-       filename:(res, file, cb) => {
-           cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-       }
-   }),
-    fileFilter: (req, file, cb)=>{
-        if(MIMETYPES.includes(file.mimetype)){ cb(null, true);
-        const fileExtension = extname(file.originalname);
-        }else{ cb(new Error(`Only ${MIMETYPES.join('')} are allowed`));
-        }
-    },
-    limits: {
-        filedSize: 10000000,
-    }
-        
-    }
-    
-    
-    );
-const express = Express();
-
-expressApp.post('/upload', multerUpload.single('myImage'), (req, res) => {
-    console.log(req.file);
-
-    res.sendStatus(200);
-});
-
-express.Application.use('/upload', express.static(join(CURRENT_DIR, '../uploads')));
-
-express.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
 
 
