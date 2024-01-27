86ae217c89931a399e95808355792fe86132559d
worldonlinegame <motar3000@hotmail.com>
2024-01-27 22:29:09 +0100

    mod reg, log, inde

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-/*
- Esta funcion carga la imagen pero no hace nada ya que la store del navegador no tiene subir imagenes
+
+ //Esta funcion carga la imagen pero no hace nada ya que la store del navegador no tiene subir imagenes
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
     window.location.href = 'login.html'
@@ -11,7 +11,47 @@ logout.addEventListener('click', ()=>{
     localStorage.removeItem('login_success')
     window.location.href = 'index.html'
 })
-*/
+/**
+ * Este es el formulario de login
+ */
+
+const loginform = document.querySelector('#loginform')
+loginform.addEventListener('submit', (e)=>{
+e.preventDefault()
+const email = document.querySelector('#email').value
+const password = document.querySelector('#password').value
+const Users = JSON.parse(localStorage.getItem('users')) || []
+const validUser = Users.find(user => user.email === email && user.password === password)
+if(!validUser){
+    return alert('Usuario y/o contraseña incorrectos!')
+}
+alert(`Bienvenid@ ${validUser.name} a la comunidad fGreat!`)
+localStorage.setItem('login_success', JSON.stringify(validUser))
+window.location.href = 'index.html'
+})
+
+/* formulario de registro*/
+
+const registroform = document.querySelector("#registroform")
+registroform.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
+
+    // creamos la base de datos en la store del navegador en un array
+
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    isUserRegistered = Users.find(user => user.email === email)
+    if(isUserRegistered){
+        return alert('El usuario ya existe!')
+    }
+    Users.push({name: name, email: email, password: password})
+    localStorage.setItem('users',JSON.stringify(Users))
+    alert('Se ha registrado satisfactoriamente!')
+    window.location.href = 'login.html'
+    
+})
 
 
 /*Crear base de datos "blog" con javascript*/
@@ -19,6 +59,7 @@ logout.addEventListener('click', ()=>{
 /*
 la url de github es: https://worldonlinegame.github.io/Web/public/fcbook.png
 */
+
 
 
 
